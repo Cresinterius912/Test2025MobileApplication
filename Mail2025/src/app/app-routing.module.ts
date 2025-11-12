ac9f1e8da7c3b785db62463fe2280d866e135c5c
Cresinterius912 <Shania.Frendo.b44065@mcast.edu.mt>
2025-11-12 14:00:23 +0100

    Mail + New Mail

---
+++ Mail2025/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/inbox',
+    pathMatch: 'full'
+  },
+  {
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'new-mail',
+    loadChildren: () => import('./new-mail/new-mail.module').then( m => m.NewMailPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
